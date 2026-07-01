33b4a9f17bbf1583ef0d0896aebd3c72e2164a88
leduycuong12345 <leduycuong56@gmail.com>
2026-07-01 21:02:46 +0700

    thuc hanh and_gate module

---
+++ Doc/OnTap/ThucHanh/and_gate_testbench.v
@@ -0,0 +1,37 @@
+`timescale 1ns/1ps
+
+module and_gate_tb;
+
+reg a;
+reg b;
+
+wire y;
+
+and_gate uut(
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    a=0;b=0;
+    #10;
+
+    a=0;b=1;
+    #10;
+
+    a=1;b=0;
+    #10;
+
+    a=1;b=1;
+    #10;
+
+    $finish;
+end
+
+initial begin
+    $monitor("time=%0t a=%b b=%b y=%b",
+                    $time,a,b,y);
+end
+
+endmodule
