60e7bbb3aca64aaf79289e37a5cbac9feb68fd80
leduycuong12345 <leduy_cuong@yahoo.com>
2026-07-07 20:16:30 +0700

    encoder only allow 1-ON trigger like 01000000 not 010100101

--- Doc/OnTap/ThucHanh/encoder_8_to_3_testbench.v
+++ Doc/OnTap/ThucHanh/encoder_8_to_3_testbench.v
@@ -10,6 +10,7 @@ initial begin
     $dumpfile("encoder_8_to_3_wave.vcd");
     $dumpvars(0,encoder_8_to_3_tb);
 end
+/*
 initial begin
     a=8'b00000000;
     #10;
@@ -561,6 +562,22 @@ initial begin
     #10;
     $finish;
 end
+*/
+
+
+initial begin
+    a = 8'b00000001; #10;
+    a = 8'b00000010; #10;
+    a = 8'b00000100; #10;
+    a = 8'b00001000; #10;
+    a = 8'b00010000; #10;
+    a = 8'b00100000; #10;
+    a = 8'b01000000; #10;
+    a = 8'b10000000; #10;
+
+    $finish;
+end
+
 initial begin
     $monitor("time=%0t a=%b y=%b",
                     $time,a,y);
